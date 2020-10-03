37300f0e121d423916fa4863fe86cf67a8c75acd
nik <niklaskrieg@tutanota.de>
2020-10-03 17:43:14 +0200

    Implemented functions to get the Spoitfy URI

--- src/commandHandler.ts
+++ src/commandHandler.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, Shard } from "discord.js";
 import { CreatePartyCommand } from "./createPartyCommand"
 import { Repository } from "./data/repository";
 import { SpotifyAPI } from "./spotify/spotifyApi";
@@ -19,9 +19,15 @@ export class CommandHandler {
     handleCommand(message : Message) {
         if (!this.isMessageBotCommand(message))
             return;
-    
+
+
+        if(this.repository.isChannelPartyChannel(message.channel.id)){
+            this.handlePartyChannelCommand(message);
+            return;
+        }    
+
         let commandParts = this.seperateCommandParts(message.content);
-    
+
         switch(commandParts.command) {
             case "createparty":
                 let instance : CreatePartyCommand = new CreatePartyCommand(message);
@@ -29,7 +35,10 @@ export class CommandHandler {
                 break;
         }
     }
-    
+
+    handlePartyChannelCommand(message: Message){
+        
+    }
 
     isMessageBotCommand(message : Message) {
         return message.channel.type == "text" && message.content.slice(0, this.commandPrefix.length - 1) == this.commandPrefix;
@@ -39,9 +48,37 @@ export class CommandHandler {
         let parts = fullComandString.toLowerCase().split(" ");
         let command = parts[0].slice(this.commandPrefix.length, parts[0].length);
         let parameters = parts.slice(1, parts.length);
-    
+
         let data = {command : command, parameters : parameters};
-        return data 
+        return data
     }
+
+
+    
+    extractSpotifyURI(spotifyLink: string): string {
+        let searchString : string = "spotify:track:";
+        let spotifyURI : string = spotifyLink.slice(spotifyLink.indexOf(searchString) + searchString.length);
+
+        return spotifyURI;
+
+    }
+
+    //gets Spotify URI from a shared Link
+    extractSpotifyURIShared(sharedLink :string): string{
+
+        let spotifyURI : string = sharedLink.slice(sharedLink.lastIndexOf("/") + 1 , sharedLink.indexOf("?"));
+
+        return  spotifyURI;
+    }
+
+    //returns true if a sharedLink contains the spotify.com domain
+    checkForSpotifyURL(sharedLink : string): boolean{
+
+        let spotifyDomain : string = "spotify.com";
+
+        return sharedLink.includes(spotifyDomain);
+    }
+
+    
 }
 
